562a3e74b0278cdf785392c6affa5c82e7c1f464
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-05 01:50:01 +0300

    Add comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = Comment.new(comment_params)
+        @comment.user = current_user
+        @comment.post_id = @post.id
+
+    if @comment.save
+        redirect_to post_path(@post), notice: 'Comment was successfully created.'
+      else
+        redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
