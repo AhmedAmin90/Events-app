20f704100c79d4fdc23fd6fb82ccd68d9a767512
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-03-04 23:39:23 +0300

    Start on posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users , :controllers => {registrations: 'registrations'}
   resources :posts do 
     resources :comments
     resources :likes
   end
   resources :events
   resources :friendships
-  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
